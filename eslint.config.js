39bcf137708fbfcfdf09d522bf5812b620ad9f0e
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-05-12 13:06:04 -0400

    use existing react app eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  env: { es2021: true, browser: true, jest: true },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  extends: 'eslint:recommended',
+  extends: ['react-app'],
 };
